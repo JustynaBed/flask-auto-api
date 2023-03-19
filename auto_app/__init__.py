6168e4037a89fc95176a82498360f5e314205169
Justyna Bednarko <justyna.bednarko@gmail.com>
2023-03-20 00:54:27 +0100

    remove comment

--- auto_app/__init__.py
+++ auto_app/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from config import Config
-from auto_app import autos
 
 app = Flask(__name__)
-app.config.from_object(Config)
+# app.config.from_object(Config)
+# configg=app.config.from_object(Config)
 
 
+from auto_app import autos
 @app.route('/')
 def index():
     return 'Hello from flask'
