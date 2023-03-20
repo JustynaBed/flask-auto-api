a528875fb2a6b00d998a8e41936f9c32d287e3ea
Justyna Bednarko <justyna.bednarko@gmail.com>
2023-03-20 01:34:04 +0100

    6

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-# import os
-# from dotenv import load_dotenv
-# from pathlib import Path
-# base_dir = Path(__file__).resolve().parent
-# env_file = base_dir / '.env'
-# load_dotenv(env_file)
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+print('sssx', os.environ.get('SECRET_KEY'))
+
 class Config:
     DEBUG = True
-    # SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
