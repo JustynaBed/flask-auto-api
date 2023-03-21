10920dfad7eac1e1c9459149ba67efc9157c8db9
Justyna Bednarko <justyna.bednarko@gmail.com>
2023-03-21 12:45:29 +0100

    8

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
-from dotenv import dotenv_values
+from dotenv import load_dotenv
 from pathlib import Path
 
 base_dir = Path(__file__).resolve().parent
 env_file = base_dir / '.env'
-dotenv_values(env_file)
+load_dotenv(env_file)
 
 
 class Config:
