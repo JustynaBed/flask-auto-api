15c5ceb4f9762d7afbb81227aa87707d3d847e12
Justyna Bednarko <justyna.bednarko@gmail.com>
2023-03-22 15:33:13 +0100

    16

--- auto_app/__init__.py
+++ auto_app/__init__.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import text
-from flask_migrate import Migrate
+# from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy.sql import text
+# from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+#
+# db = SQLAlchemy(app)
+# migrate = Migrate(app, db)
 #
 # with app.app_context():
 #     results = db.session.execute(text('show databases'))
