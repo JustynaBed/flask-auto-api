8b058c0fa60cec6a89e2d7990220943d9b79a0ce
Justyna Bednarko <justyna.bednarko@gmail.com>
2023-03-22 15:48:08 +0100

    15

--- auto_app/__init__.py
+++ auto_app/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from config import Config
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 # from sqlalchemy.sql import text
 # from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 #
-# db = SQLAlchemy(app)
+db = SQLAlchemy(app)
 # migrate = Migrate(app, db)
 #
 # with app.app_context():
