d08aba1ea0ab059ded90ad43c4303d19693e604b
Justyna Bednarko <justyna.bednarko@gmail.com>
2023-03-22 12:58:33 +0100

    12

--- auto_app/__init__.py
+++ auto_app/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+
+with app.app_context():
+    results = db.session.execute(text('show databases'))
+for row in results:
+    print(row)
 
 
 from auto_app import autos
